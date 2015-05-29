c064fd1ee09f2af6069d280f935332b1bb2fb541
inndee <inndeemercado@gmail.com>
2015-05-29 09:35:01 +0800

    Initial commit

---
+++ www/js/appframe.js
@@ -0,0 +1,54 @@
+var XMLTools = 
+{
+	domParser : function (xml_string) {
+		var xmldoc;
+		if (window.DOMParser) {
+			parser = new DOMParser();
+			xmldoc = parser.parseFromString(xml_string, "text/xml");
+		} else // Internet Explorer
+		{
+			xmldoc = new ActiveXObject("Microsoft.XMLDOM");
+			xmldoc.async = false;
+			xmldoc.loadXML(xml_string);
+		}
+		return xmldoc;
+	}
+};
+
+
+
+var Logging = {
+	debug : function ( message ){
+		console.log ( " -- [DEBUG]: " + message );
+	},
+	trace : function ( message ){
+		console.log ( " -- [TRACE]: " + message );
+	},
+	error : function ( message ){
+		console.log ( " -- [ERROR]: " + message );
+	}
+};
+
+
+var FileTools = {
+	
+	fileToString : function ( filename )
+	{
+		var rawFile = new XMLHttpRequest();
+		rawFile.open("GET", filename, true );
+		rawFile.onreadystatechange = function ()
+		{
+			if(rawFile.readyState === 4)
+			{
+				if(rawFile.status === 200 || rawFile.status == 0)
+				{
+					var allText = rawFile.responseText;
+					return allText;
+				}
+			}
+		}
+		rawFile.send(null);
+	}
+	
+}
+
